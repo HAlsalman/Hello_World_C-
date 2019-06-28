a3b1cfb777913d7035e3357020f87bbf5c0e46ac
Hassan Alsalman <45472463+HAlsalman@users.noreply.github.com>
2019-06-27 19:04:27 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Simple C++ program to display "Hello World".
+  
+#include<iostream>  // Header file for input output functions.
+
+using namespace std; 
+ 
+int main() {
+    cout<<"Hello World";     // prints hello world.
+    return 0; 
+}
